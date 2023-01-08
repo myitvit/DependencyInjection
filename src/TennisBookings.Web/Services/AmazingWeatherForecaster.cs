174769257352c3c6f4e1c9868ee3af20fcdb1f5c
Vitali Prakapenka <myitvit@gmail.com>
2023-01-08 11:56:16 +0300

    Added another weather forecaster class

---
+++ src/TennisBookings.Web/Services/AmazingWeatherForecaster.cs
@@ -0,0 +1,15 @@
+﻿namespace TennisBookings.Web.Services
+{
+    public class AmazingWeatherForecaster : IWeatherForecaster
+    {
+        public WeatherResult GetCurrentWeather()
+        {
+            // DO SOMETHING AMAZING HERE!!!
+
+            return new WeatherResult
+            {
+                WeatherCondition = WeatherCondition.Sun
+            };
+        }
+    }
+}
