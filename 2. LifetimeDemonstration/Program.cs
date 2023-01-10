41436944f7c991fd8a5df64ea15fb926d7892ece
Vitali Prakapenka <myitvit@gmail.com>
2023-01-10 16:03:09 +0300

    Enable Dependancies Scope Validation

--- 2. LifetimeDemonstration/Program.cs
+++ 2. LifetimeDemonstration/Program.cs
@@ -5,13 +5,17 @@ namespace LifetimeDemonstration.Web
 {
     public class Program
     {
-        public static void Main (string[] args)
+        public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseDefaultServiceProvider(options =>
+                {
+                    options.ValidateScopes = true;
+                })
                 .UseStartup<Startup>();
     }
 }
